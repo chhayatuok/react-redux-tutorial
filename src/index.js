599346170195e0c3535f91d4f3aadb1acfcfa2d6
c t <20270456+chhayatuok@users.noreply.github.com>
2019-11-24 11:34:55 -0500

    Added selector. Enabled Redux-DevTool

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store'
-
+import { getReadableStories } from './selectors/story';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
-const stories = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-        }, {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
-
-
 ReactDOM.render(
-    <App stories={store.getState().storyState} onArchive={() => {}} />,
+    <App
+        stories={getReadableStories(store.getState())}
+        onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+    />,
     document.getElementById('root')
 );
 
